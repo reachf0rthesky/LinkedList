4e977ed976b650b7d2ec892363efecc472e045ae
reachf0rthesky <122790132+reachf0rthesky@users.noreply.github.com>
2023-01-16 12:50:59 +0100

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+        public int int_value = 0;
+        public Node next_node = null;
+
+        public Node(int int_value) {
+            this.int_value = int_value;
+        }
+
+        public Node(int int_value, Node next_node) {
+            this.next_node = next_node;
+            this.int_value = int_value;
+        }
+
+        public Node lastNode() {
+            Node tmp = this;
+            while (tmp.next_node != null) {
+                tmp = tmp.next_node;
+            }
+
+            return tmp;
+        }
+
+        public void insert(Node insertThis) {
+            this.lastNode().next_node = insertThis;
+        }
+
+        public bool delete(int index) {
+            if (index <= 0)
+                return false;
+
+            Node tmp = this;
+            Node tmpNextOne=this.next_node;
+
+            for (int i = 1; i < index-1; i++){
+                if (this.next_node == null)
+                    return false;
+                tmp = tmp.next_node;
+                tmpNextOne = tmp.next_node;
+            }
+
+            tmp.next_node = tmpNextOne.next_node;
+            return true;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node1 = new Node(5);
+
+            node1.insert(new Node(12));
+
+            node1.insert(new Node(13));
+
+            Node tmp = node1;
+
+            while (tmp.next_node != null)
+            {
+
+                Console.WriteLine(tmp.int_value);
+
+                tmp= tmp.next_node;
+
+                if(tmp.next_node == null)
+                {
+                    Console.WriteLine(tmp.int_value);
+                }
+
+            }
+
+            node1.delete(2);
+
+            tmp = node1;
+
+            while (tmp.next_node != null)
+            {
+
+                Console.WriteLine(tmp.int_value);
+
+                tmp = tmp.next_node;
+
+                if (tmp.next_node == null)
+                {
+                    Console.WriteLine(tmp.int_value);
+                }
+
+            }
+
+
+
+        }
+    }
+}
